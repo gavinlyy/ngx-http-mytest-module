513b743ff37a3af8975768b40639f61ca2aa3d46
Gavin <gavinlyy@foxmail.com>
2015-05-24 17:02:27 +0800

    fix bug

--- src/ngx_http_mytest_module.c
+++ src/ngx_http_mytest_module.c
@@ -45,11 +45,65 @@ static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t* r)
 	return ngx_http_output_filter(r, &out);
 }
 
+static ngx_int_t ngx_http_mytest_handler2(ngx_http_request_t* r){
+	if(!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))){
+		return NGX_HTTP_NOT_ALLOWED;
+	}
+	ngx_int_t rc = ngx_http_discard_request_body(r);
+	if(rc != NGX_OK){
+		return rc;
+	}
+	ngx_buf_t *b;
+	b = ngx_palloc(r->pool, sizeof(ngx_buf_t));
+	u_char* filename = (u_char*)"/var/www/html/index.html";
+	b->in_file = 1;
+	b->file = ngx_palloc(r->pool, sizeof(ngx_file_t));
+	b->file->fd = ngx_open_file(filename, NGX_FILE_RDONLY | NGX_FILE_NONBLOCK, NGX_FILE_OPEN, 0);
+	b->file->log = r->connection->log;
+	b->file->name.data = filename;
+	b->file->name.len = sizeof(filename) - 1;
+	if(b->file->fd <= 0){
+		return NGX_HTTP_NOT_FOUND;
+	}
+	if(ngx_file_info(filename, &b->file->info) == NGX_FILE_ERROR){
+		return NGX_HTTP_INTERNAL_SERVER_ERROR;
+	}	
+	b->file_pos = 0;
+	b->file_last = b->file->info.st_size;
+	
+	ngx_str_t type = ngx_string("text/html");
+	r->headers_out.status = NGX_HTTP_OK;
+	r->headers_out.content_length_n = b->file->info.st_size;
+	r->headers_out.content_type = type;
+	rc = ngx_http_send_header(r);
+	if(rc == NGX_ERROR || rc > NGX_OK || r->header_only){
+		return rc;
+	}
+	
+	ngx_chain_t out;
+	out.buf = b;
+	out.next = NULL;
+	return ngx_http_output_filter(r, &out);
+}
+
+/*
+#define ngx_http_conf_get_module_loc_conf(cf, module) \
+    ((ngx_http_conf_ctx_t *) cf->ctx)->loc_conf[module.ctx_index]
+*/
 static char* ngx_http_mytest(ngx_conf_t* cf, ngx_command_t* cmd, void* conf)
 {
+	if(cf->args->nelts < 1){
+		return NGX_CONF_ERROR;
+	}
 	ngx_http_core_loc_conf_t* clcf;
 	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-	clcf->handler = ngx_http_mytest_handler;
+	ngx_str_t* str = cf->args->elts;
+	if(ngx_strncmp(str[1].data, "helloworld", str[1].len) == 0)
+		clcf->handler = ngx_http_mytest_handler;
+	else if(ngx_strncmp(str[1].data, "sendfile", str[1].len) == 0)
+		clcf->handler = ngx_http_mytest_handler2;
+	else
+		return NGX_CONF_ERROR;
 	return NGX_CONF_OK;
 } 
 /*
@@ -69,7 +123,7 @@ struct ngx_command_s{
 static ngx_command_t ngx_http_mytest_commands[] = {
 	{
 		ngx_string("mytest"),
-		NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+		NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_TAKE1,
 		ngx_http_mytest,
 		NGX_HTTP_LOC_CONF_OFFSET,
 		0,
